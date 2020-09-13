d6bf9aa62efae33d67a502a802bc014d6dd13821
3madov-77 <emadzxy7@gmail.com>
2020-09-13 12:04:48 +0300

    under devo..

--- price_comparision_extension/app.py
+++ price_comparision_extension/app.py
@@ -4,20 +4,60 @@ from modules.newegg import newegg
 from modules.walmart import walmart
 import json
 import csv
+import datetime
+
 
 
 
 def get_prices():
 
-    users = None
+    user = None
+    now = datetime.datetime.now()
 
-    def sign_up():
-        with open('users.csv') as csv_file:
-        # df = pd.read_csv("../users.csv")
-        # exists = df[df.user == user].user.item()
+    def what_do_u_want():
+        choice = input(f'what do you want to do {user}?\n - (n)ew search?\n - (v)iew my favorite?\n - (s)ign out\n > ').lower()
+        if choice == 'n':
+            new_search()
+        elif choice == 'v':
+            view_my_fav()
+        elif choice == 's':
+            main_nav()
+
+    def new_search():
+        product = input('what are you looking for? : ')
+        print('plaese wait...')
+        all_objects = e_bay(product)
+        newlist = sorted(all_objects, key=lambda x: x.price)
+        for i in range(len(newlist)):
+            print(i+1, newlist[i])
+        choice_pro = int(input('witch one you want to save? : '))
+        print(newlist[choice_pro-1])
+
+        with open('../saves.csv', 'a', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerows([[user,newlist[choice_pro-1].title,newlist[choice_pro-1].link, newlist[choice_pro-1].website, newlist[choice_pro-1].price, now]])
+
+
+    def view_my_fav():
+        with open('../saves.csv', 'r') as csv_file:
+
             csv_reader = csv.reader(csv_file, delimiter=',')
             new_list = [[ii for ii in i] for i in csv_reader]
-            # x = True
+
+            y = False
+            for i in new_list:
+                if user == i[0]:
+                    print(f'{i[1]} from {i[3]} and it was {i[4]}$ , it had been add in {i[5]}\n')
+                    y = True
+            if not y:
+                print('your favorite still empty.. :(')
+    
+    def sign_up():
+        with open('../users.csv', 'r') as csv_file:
+
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            new_list = [[ii for ii in i] for i in csv_reader]
+
             while True:
                 user = input('what is your name? : ')
                 y = False
@@ -26,43 +66,46 @@ def get_prices():
                         print('the name is already taken!!')
                         y = True
                 if y:
-
                     continue
                 else:
                     break
 
             password = input('your password : ')
-        # with open('users.csv', 'a', newline='') as file:
-            writer = csv.writer(csv_file, delimiter=',')
-            writer.writerows([[user, password]])
-            csv_file.close()
-        choice = input('what do want to do?\n - (n)ew search?\n - (v)iew my favorite? ').lower()
-        if choice == 'n':
-            new_search()
-        elif choice == 'v':
-            # view_my_fav()
-    
-    def new_search():
-        product = input('what are you looking for? : ')
-        all_objects = e_bay(product)
-        newlist = sorted(all_objects, key=lambda x: x.price)
-        for i in range(len(newlist)):
-            print(i+1, newlist[i])
-        choice_pro = int(input('witch one you want to save? : '))
-        print(newlist[choice_pro-1])
-
-        with open('saves.csv', 'a', newline='') as file:
+        with open('../users.csv', 'a', newline='') as file:
             writer = csv.writer(file, delimiter=',')
-            # writer.writerows([[user,newlist[choice_pro-1].link, newlist[choice_pro-1].website]])s
+            writer.writerows([[user, password]])
+            file.close()
+        what_do_u_want()
 
-    while True:
-        first = input('in or up?').lower()
-        if first == 'in':
-            # sign_in()
-            break
-        elif first == 'up':
-            sign_up()
-            break
+    
+    def sign_in():
+        with open('../users.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            new_list = [[ii for ii in i] for i in csv_reader]
+
+        while True:
+                user = input('what is your name? : ')
+                y = False
+                for i in new_list:
+                    if user in i:
+                        print(f'hello {user}.. :)')
+                        y = True
+                if y:
+                    break
+                else:
+                    print('user not found!')
+                    continue
+        what_do_u_want()
+
+    def main_nav():
+        while True:
+            first = input('in or up?').lower()
+            if first == 'in':
+                sign_in()
+                break
+            elif first == 'up':
+                sign_up()
+                break
 
 # def save_choice(list):
 
