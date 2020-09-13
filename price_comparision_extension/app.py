4654468b3578dd6039356eff9eb5c539aa8b49bd
3madov-77 <emadzxy7@gmail.com>
2020-09-13 17:43:12 +0300

    under devolp

--- price_comparision_extension/app.py
+++ price_comparision_extension/app.py
@@ -5,6 +5,7 @@ from modules.walmart import walmart
 import json
 import csv
 import datetime
+import sys
 
 
 user = None
@@ -15,29 +16,37 @@ def get_prices():
     # now = datetime.datetime.now()
 
     def what_do_u_want():
-        choice = input(f'what do you want to do {user}?\n - (n)ew search?\n - (v)iew my favorite?\n - (s)ign out\n > ').lower()
-        if choice == 'n':
-            new_search()
-        elif choice == 'v':
-            view_my_fav()
-        elif choice == 's':
-            main_nav()
+        choice = input(f'\n\nwhat do you want to do {user}?\n - (n)ew search?\n - (v)iew my favorite?\n - (s)ign out\n - (ex)it\n > ').lower()
+        while choice != 'ex':
+            if choice == 'n':
+                new_search()
+            elif choice == 'v':
+                view_my_fav()
+            elif choice == 's':
+                main_nav()
+            
+            choice = input(f'\n\nwhat do you want to do {user}?\n - (n)ew search?\n - (v)iew my favorite?\n - (s)ign out\n - (ex)it\n > ').lower()
 
     def new_search():
         global user
-        product = input('what are you looking for? : ')
-        print('plaese wait...')
+        product = input('\nwhat are you looking for? : ')
+        print('plaese wait...\n')
         all_objects = e_bay(product)
         newlist = sorted(all_objects, key=lambda x: x.price)
         for i in range(len(newlist)):
             print(i+1, newlist[i])
-        choice_pro = int(input('witch one you want to save? : '))
-        print(newlist[choice_pro-1])
+        sure = input('want to save any of them?').lower()
+        if sure == 'y':
+            choice_pro = int(input('witch one you want to save? : '))
+            print(newlist[choice_pro-1])
 
-        with open('../saves.csv', 'a', newline='') as file:
-            writer = csv.writer(file, delimiter=',')
-            writer.writerows([[user,newlist[choice_pro-1].title,newlist[choice_pro-1].link, newlist[choice_pro-1].website, newlist[choice_pro-1].price, datetime.datetime.now()]])
-        what_do_u_want()
+            with open('../saves.csv', 'a', newline='') as file:
+                writer = csv.writer(file, delimiter=',')
+                writer.writerows([[user,newlist[choice_pro-1].title,newlist[choice_pro-1].link, newlist[choice_pro-1].website, newlist[choice_pro-1].price, datetime.datetime.now()]])
+            print('\n add to your favorite.. :)')
+            what_do_u_want()
+        elif sure == 'n':
+            what_do_u_want()
 
 
     def view_my_fav():
@@ -47,16 +56,22 @@ def get_prices():
 
             csv_reader = csv.reader(csv_file, delimiter=',')
             new_list = [[ii for ii in i] for i in csv_reader]
+            
+            last_list = []
+            for row in csv_reader:
+                last_list.append(row)
+                for field in row:
+                    if field == user:
+                        last_list.remove(row)
 
         while choice != 'n':
 
             print('\n\nproduct id , -------product name------- , ---website--- , ---price--- , ---date--- \n')
             y = False
-            # print(new_list)
-            # print(user)
+
             for i in range(len(new_list)):
                 if user == new_list[i][0]:
-                    print(f'{i}, {new_list[i][1]} from {new_list[i][3]} and it was {new_list[i][4]}$ , last cheak was in: {new_list[i][5]}')
+                    print(f'{i}, "{new_list[i][1]}" from "{new_list[i][3]}" and it was "{new_list[i][4]}$" , last cheak was in: "{new_list[i][5]}""')
                     y = True
             if not y:
                 print('your favorite still empty.. :(')
@@ -83,12 +98,18 @@ def get_prices():
                 # new_list[choice_pro_id][4],new_list[choice_pro_id][5] = new_line,datetime.datetime.now()
                 # continue
 
-                
+        with open('../saves.csv', 'w') as writeFile:
+
+            writer = csv.writer(writeFile)
+
+            writer.writerows(last_list)   
+            writer.writerows(new_list)   
 
         what_do_u_want()
     
 
     def sign_up():
+        global user
         with open('../users.csv', 'r') as csv_file:
 
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -121,12 +142,17 @@ def get_prices():
             new_list = [[ii for ii in i] for i in csv_reader]
 
         while True:
-                user = input('what is your name? : ')
+                user = input('\nwhat is your name? : ')
                 y = False
                 for i in new_list:
                     if user in i:
-                        print(f'hello {user}.. :)')
-                        y = True
+                        password = input('your password : ')
+                        if password == i[1]:
+                            print(f'hello {user}.. :)')
+                            y = True
+                        else:
+                            print('wrong password!!')
+                            continue
                 if y:
                     break
                 else:
@@ -136,13 +162,14 @@ def get_prices():
 
     def main_nav():
         while True:
-            first = input('in or up?').lower()
+            first = input('\nin or up?').lower()
             if first == 'in':
                 sign_in()
                 break
             elif first == 'up':
                 sign_up()
                 break
+    print('\nWelcome to Price-Compersion-App\n')
     main_nav()
 # def save_choice(list):
 
