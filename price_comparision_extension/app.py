6c1daaeb0980cedfbff024fde0af4142dc873281
3madov-77 <emadzxy7@gmail.com>
2020-09-13 18:29:59 +0300

    under devolp

--- price_comparision_extension/app.py
+++ price_comparision_extension/app.py
@@ -13,3 +13,18 @@ def get_prices():
 
     
     # now = datetime.datetime.now()
+
+    def what_do_u_want():
+        choice = input(f'\n\nwhat do you want to do {user}?\n - (n)ew search?\n - (v)iew my favorite?\n - (s)ign out\n - (ex)it\n > ').lower()
+        while True:
+            if choice == 'n':
+                new_search()
+                break
+            elif choice == 'v':
+                view_my_fav()
+                break
+            elif choice == 's':
+                main_nav()
+                break
+            elif choice == 'ex':
+ 
